c1eca484725cae8be7e7e2f93b9298d3eb690c65
Den255 <abr.den255@gmail.com>
2022-06-14 13:59:55 +0300

    add u.gg datasourse

---
+++ src/main/java/org/openjfx/UGGSource.java
@@ -0,0 +1,35 @@
+package org.openjfx;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+public class UGGSource {
+    private static final String UGG_VERSION = "1.1";
+    private static final String BASE_URL = "https://stats2.u.gg/lol/" + UGG_VERSION + "/table/runes/10_14/ranked_solo_5x5/%championId%/1.4.0.json";
+    public JsonObject getRunes(int championId) throws IOException {
+        //Example: https://stats2.u.gg/lol/1.1/table/runes/10_14/ranked_solo_5x5/103/1.4.0.json
+        URL url = new URL(BASE_URL
+                .replace("%championId%", Integer.toString(championId)));
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        //int status = con.getResponseCode();
+        InputStreamReader streamReader = new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8);
+        String runesStr = new BufferedReader(streamReader).lines().collect(Collectors.joining("\n"));
+        con.disconnect();
+
+        JsonParser parser = new JsonParser();
+        Object obj = parser.parse(runesStr);
+        JsonObject jsonObj = (JsonObject)obj;
+        JsonObject plRunes = jsonObj.get("10").getAsJsonObject(); // PLATINUM_PLUS
+
+        return plRunes.get("12").getAsJsonObject(); // World
+    }
+}
